94fe7d5a7cc96f5b45de41346c2486088405006e
mikefallon <mikefallon81@gmail.com>
2019-09-16 21:40:06 +0100

    MVP complete

---
+++ src/main/java/com/codeclan/example/pirateservice_d1_starter/models/Ship.java
@@ -0,0 +1,60 @@
+package com.codeclan.example.pirateservice_d1_starter.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="ships")
+
+public class Ship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "ship")
+    private List<Pirate> pirates;
+
+
+    public Ship(String name ) {
+        this.name = name;
+        this.pirates = new ArrayList<>();
+
+    }
+
+    public Ship() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Pirate> getPirates() {
+        return pirates;
+    }
+
+    public void setPirates(List<Pirate> pirates) {
+        this.pirates = pirates;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
